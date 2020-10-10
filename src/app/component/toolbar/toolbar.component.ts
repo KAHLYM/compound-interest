fbf8b3db10293d626124563c70152ee5ee37eb7f
KAHLYM <wrightci01w@gmail.com>
2020-10-10 16:34:45 +0100

    Fix toolbar shadow

--- src/app/component/toolbar/toolbar.component.ts
+++ src/app/component/toolbar/toolbar.component.ts
@@ -11,13 +11,16 @@ export class ToolbarComponent implements OnInit {
   constructor(
     private scrollDispactcher: ScrollDispatcher) {
 
-      this.scrollDispactcher.scrolled().subscribe(ret => {
-        var toolbar: HTMLElement = document.querySelector('#toolbar');
-        if (window.scrollY > toolbar.clientHeight) {
-          toolbar.style.boxShadow = "0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)";
-        }
-      })
-    }
+    this.scrollDispactcher.scrolled().subscribe(() => {
+      var toolbar: HTMLElement = document.querySelector('#toolbar');
+      if (window.scrollY > toolbar.clientHeight) {
+        toolbar.style.boxShadow = "0 3px 5px -1px rgba(0,0,0,.2), 0 6px 10px 0 rgba(0,0,0,.14), 0 1px 18px 0 rgba(0,0,0,.12)";
+      } else {
+        var perc = 1 / toolbar.clientHeight * window.scrollY;
+        toolbar.style.boxShadow = "0 3px 5px -1px rgba(0,0,0," + (perc * 0.2).toString() + "), 0 6px 10px 0 rgba(0,0,0," + (perc * 0.14).toString() + "), 0 1px 18px 0 rgba(0,0,0," + (perc * 0.12).toString() + ")";
+      }
+    })
+  }
 
-    ngOnInit() { }
+  ngOnInit() { }
 }
